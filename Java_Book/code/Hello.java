cce8e57735c8caf4b4f0aaf71e74c8525610a487
taehee <168088580+hea97@users.noreply.github.com>
2024-06-14 22:02:00 +0900

    Create Hello.java

---
+++ Java_Book/code/Hello.java
@@ -0,0 +1,11 @@
+package sec01;
+
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.println("Hello"+"World");
+
+    String Hello = "안녕";
+    System.out.println(Hello);
+  }
+}
