f472202fad10f66634ea14de7798d65d4fd2dce1
taehee <168088580+hea97@users.noreply.github.com>
2024-06-18 20:41:58 +0900

    Create ScannerDemo.java

---
+++ Java_Book/code/ScannerDemo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ScannerDemo {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(system.in);
+    int x = in.nextINt();
+    int y = in.nextINt();
+    system.out.printf("%d * %d은 %d입니다.\n", x, y, x * y);
+  }
+}
+
+// 실행 결과
+// 10
+// 20
+// 10 * 20은 200입니다.
