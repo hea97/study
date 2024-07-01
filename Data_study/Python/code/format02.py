b55da9d20c4dec896e4bd1c8bf6cad60bbb93b65
taehee <168088580+hea97@users.noreply.github.com>
2024-07-01 11:26:05 +0900

    Update format02.py

--- Data_study/Python/code/format02.py
+++ Data_study/Python/code/format02.py
@@ -7,4 +7,13 @@ output_c = "{:10d}".format(52)
 
 # 빈칸 0 채우기
 output_d = "{:05d}".format(52)
-output_c = "{:05d}".format(-52)
+output_e = "{:05d}".format(-52)
+
+print("# 기본")
+print(output_a)
+print("# 특정 칸에 출력")
+print(output_b)
+print(output_c)
+print("# 빈칸 0 채우기")
+print(output_d)
+print(output_e)
