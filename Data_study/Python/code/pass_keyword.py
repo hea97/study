44b78aa5c30c2eb34ba15f558792c77bc98620d8
taehee <168088580+hea97@users.noreply.github.com>
2024-07-01 12:24:49 +0900

    Create pass_keyword.py

---
+++ Data_study/Python/code/pass_keyword.py
@@ -0,0 +1,11 @@
+# 입력 받기
+number = input("정수 입력> ")
+nubmer = int(number)
+
+# 조건문 사용
+if unmber > 0:
+  # 양수일 때 아직 미구현
+else: 
+  # 음수일 때 아직 미구현
+
+# IndentationErorr: expected an indented block error 발생
