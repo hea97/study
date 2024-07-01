3e3073e100b3085fb37d8c02dfc72dc4a310f132
taehee <168088580+hea97@users.noreply.github.com>
2024-07-01 11:23:55 +0900

    Create format02.py

---
+++ Data_study/Python/code/format02.py
@@ -0,0 +1,10 @@
+# 정수
+output_a = "{: d}".format(52)
+
+# 특정 칸에 출력
+output_b = "{:5d}".format(52)
+output_c = "{:10d}".format(52)
+
+# 빈칸 0 채우기
+output_d = "{:05d}".format(52)
+output_c = "{:05d}".format(-52)
