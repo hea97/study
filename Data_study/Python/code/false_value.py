b2c4c038e6d262715252ed44e95f30dda8784ffe
taehee <168088580+hea97@users.noreply.github.com>
2024-07-01 12:23:03 +0900

    Create false_value.py

---
+++ Data_study/Python/code/false_value.py
@@ -0,0 +1,12 @@
+print("# if 조건문 0넣기")
+if 0:
+  print("0은 True로 변환")
+else:
+  print("0은 False로 변환")
+  print()
+
+print("# if 조건문에 빈 문자열 넣기")
+if "":
+  print("빈 문자열 True로 변환")
+else:
+  print("빈 문자열 False로 변환")
