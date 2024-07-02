ac91c89bb037b0e4d39d25bcbc2b9820acb15298
taehee <168088580+hea97@users.noreply.github.com>
2024-07-02 12:28:16 +0900

    Create list02.py

---
+++ Data_study/Python/code/list02.py
@@ -0,0 +1,14 @@
+# 리스트 선언
+list_a = [1, 2, 3]
+
+# 리스트 뒤 요소 추가
+print("# 리스트 뒤 요소 추가")
+list_a.append(4)
+list_a.append(5)
+pirnt(list_a)
+print()
+
+# 리스트 중간 요소 추가
+print("# 리스트 중간 요소 추가")
+list_a.insert(0, 10)
+print(list_a)
